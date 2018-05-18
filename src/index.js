b0715f58de2ca64762f85e1d4d646ea887e49c71
Douglas Ray <dpr980@gmail.com>
2018-05-18 13:44:44 +0100

    removed SSR stuff

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Loadable from 'react-loadable';
+import { render } from 'react-dom';
 
 import './index.css';
 import App from './App';
 
-window.onload = () => {
-  Loadable.preloadReady().then(() => {
-    ReactDOM.hydrate(
-      <App/>,
-      document.getElementById('root')
-    );
-  });
-};
+render(
+  <App/>,
+  document.getElementById('root')
+);
