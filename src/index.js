d89eac9e9a126c08694be39cff85681c844eee49
Douglas Ray <dpr980@gmail.com>
2018-05-18 13:09:59 +0100

    added SSR

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Loadable from 'react-loadable';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+window.onload = () => {
+  Loadable.preloadReady().then(() => {
+    ReactDOM.hydrate(
+      <App/>,
+      document.getElementById('root')
+    );
+  });
+};
